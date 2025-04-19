3ceafda941f6e36282fec61f095914538ed56621
Suryarpan <52556381+Suryarpan@users.noreply.github.com>
2025-04-20 02:07:04 +0530

    added error encoders

--- utils/resp_encoder.go
+++ utils/resp_encoder.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func Encode[T any](w http.ResponseWriter, status int, v T) error {
@@ -14,4 +16,33 @@ func Encode[T any](w http.ResponseWriter, status int, v T) error {
 		return fmt.Errorf("could not encode to json: %w", err)
 	}
 	return nil
+}
+
+func EncodeError[T any](w http.ResponseWriter, status int, data T) error {
+	var statusMessage string
+	if 400 <= status && status <= 499 {
+		statusMessage = "bad request received"
+	} else if 500 <= status && status <= 599 {
+		statusMessage = "server unable to process"
+	} else {
+		return fmt.Errorf("bad status code provided %d", status)
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(status)
+
+	mssg := map[string]any{"message": statusMessage, "reason": data}
+	err := json.NewEncoder(w).Encode(mssg)
+	if err != nil {
+		return fmt.Errorf("could not encode to json: %w", err)
+	}
+	return nil
+}
+
+func EncodeValidationError(w http.ResponseWriter, ves validator.ValidationErrors) {
+	errorMssgs := make(map[string]map[string]any)
+	for _, ve := range ves {
+		errorMssgs[ve.Field()] = map[string]any{"value": ve.Value(), "error": ve.Error()}
+	}
+	EncodeError(w, http.StatusBadRequest, errorMssgs)
 }
